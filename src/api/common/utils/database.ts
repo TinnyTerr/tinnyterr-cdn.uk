54aaaac9cadf8579eb04f4515e4252cf51aefa33
tinnyterr <168141209+TinnyTerr@users.noreply.github.com>
2025-03-23 11:32:33 +0000

    add a database class for easier use
    Signed-off-by: tinnyterr <168141209+TinnyTerr@users.noreply.github.com>

--- src/api/common/utils/database.ts
+++ src/api/common/utils/database.ts
@@ -1,10 +1,16 @@
 import dotenv from "dotenv";
-import { MongoClient } from "mongodb";
+import {
+	type CollectionOptions,
+	type Db,
+	MongoClient,
+	type WithId,
+	type WithoutId,
+} from "mongodb";
 
 dotenv.config();
 
 const client = new MongoClient(
-	process.env.MONGODB_URI || "mongodb://localhost:27017",
+	process.env.MONGODB_URI ?? "mongodb://localhost:27017",
 );
 export const db = client.db("file_hoster_new");
 
@@ -16,3 +22,25 @@ export const db = client.db("file_hoster_new");
 		console.error("Failed to connect to database", error);
 	}
 })();
+
+// Capitalised to allow for backporting to previous use
+export class DB {
+	private connection: Db;
+	private connectionUri: string;
+	constructor() {
+		this.connectionUri = process.env.MONGODB_URI ?? "mongodb://localhost:27017";
+		this.connection = new MongoClient(this.connectionUri).db("file_hoster_new");
+	}
+
+	public collection(name: string, options?: CollectionOptions) {
+		return this.connection.collection(name, options);
+	}
+
+	// biome-ignore lint/suspicious/noExplicitAny: idk
+	public removeId<T extends WithId<{ [key: string]: any }>>(
+		object: T,
+	): WithoutId<T> {
+		const { _id, ...rest } = object;
+		return { ...rest } as WithoutId<T>;
+	}
+}
